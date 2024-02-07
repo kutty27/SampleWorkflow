0c3b0e8b0be4de24cbf566ad485622ed5dd56198
kutty27 <154492609+kutty27@users.noreply.github.com>
2024-02-07 16:01:13 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String [] args){
+    Car c = new Car("Maruthi-Suzuki", "Black",120000);
+    System.out.println("Car Name : "+c.getCarName());
+    System.out.println("Car Color: "+c.getCarColor());
+    System.out.println("Car Price : "+c.getCarPrice());
+  }
+}
